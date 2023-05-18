fd283c4e92810738f7cb297183b1ba41dc6f97c1
Santiago <santimir12@gmail.com>
2023-05-18 01:07:35 -0300

    Ejemplos de la guie

---
+++ EGG/JAVAINTRO 01/src/javaintro/pkg01/JAVAINTRO01.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaintro.pkg01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author santi
+ */
+public class JAVAINTRO01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in); 
+        int numeros;
+        
+        do {
+               System.out.println(" Ingrese números del 1 al 20 ");
+                numeros = leer.nextInt();
+               
+        
+        }while (numeros != 20);  {
+        
+    }
+               
+                       
+   
+        
+        /* switch (motor) {
+            
+            case 1: System.out.println(" La bomba es una de agua");
+            break;
+            
+            case 2: System.out.println(" La bomba es una de gasolina");
+            break;
+            
+            case 3: System.out.println(" La bomba es una de hormigon");
+            break;
+            
+            case 4: System.out.println(" La bomba ees una de pasta alimenticia");
+            break;
+            
+            default: System.out.println(" No existe un valor valido para el tipo de bomba");  
+        }
+      */ 
+    }
+
+
+        
+}
+    
+
